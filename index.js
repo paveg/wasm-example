c4ed9668d87ef03577373227bebef44449606dc5
Ryota Ikezawa <pavegy@gmail.com>
2019-08-28 01:10:06 +0900

    :sparkles: Add console.log function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const rust = import("./pkg/wasm_example");
 
+rust.then(m => m.run()).catch(console.error);
 rust.then(m => m.greet("World!")).catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,17 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn run() {
+    log("Hello logging!");
 }
 
